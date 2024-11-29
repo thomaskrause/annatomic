f8133af188beba4cd45c79c1e2f340cd0bcbdea6
Thomas Krause <thomaskrause@posteo.de>
2024-11-29 13:18:23 +0100

    Start to show a token demo

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "Annatomic",
         native_options,
-        Box::new(|cc| Ok(Box::new(annatomic::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(annatomic::AnnatomicApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(annatomic::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(annatomic::AnnatomicApp::new(cc)))),
             )
             .await;
 
