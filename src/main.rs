1b2fd93089f4ec647c888382d2b54ad2bc54dcb9
Thomas Krause <thomas.krause@hu-berlin.de>
2024-12-04 11:54:38 +0100

    Set application ID

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() -> eframe::Result {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0])
+            .with_app_id("annatomic")
             .with_icon(
                 // NOTE: Adding an icon is optional
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
