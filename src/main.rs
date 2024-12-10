e88a83cd0459d4b81c01c5ae3b48100de8eb4528
Thomas Krause <thomas.krause@hu-berlin.de>
2024-12-10 12:38:33 +0100

    Use wayland feature of egui, so the icon is displayed properly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use annatomic::AnnatomicApp;
+
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
@@ -16,9 +18,13 @@ fn main() -> eframe::Result {
             ),
         ..Default::default()
     };
+
     eframe::run_native(
-        "Annatomic",
+        "annatomic",
         native_options,
-        Box::new(|cc| Ok(Box::new(annatomic::AnnatomicApp::new(cc).unwrap()))),
+        Box::new(|cc| {
+            let app = AnnatomicApp::new(cc)?;
+            Ok(Box::new(app))
+        }),
     )
 }
