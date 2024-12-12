647e170a19b7c2502085166f5d41cefd2fe925c4
Thomas Krause <thomas.krause@hu-berlin.de>
2024-12-12 15:33:04 +0100

    Only show framerate when program is started with the "--dev" argument

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use annatomic::AnnatomicApp;
+use annatomic::{AnnatomicApp, AnnatomicArgs};
+use clap::Parser;
 
 fn main() -> eframe::Result {
+    let args = AnnatomicArgs::parse();
+
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
@@ -16,6 +19,7 @@ fn main() -> eframe::Result {
                 eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
                     .expect("Failed to load icon"),
             ),
+        persist_window: true,
         ..Default::default()
     };
 
@@ -23,7 +27,7 @@ fn main() -> eframe::Result {
         "annatomic",
         native_options,
         Box::new(|cc| {
-            let app = AnnatomicApp::new(cc)?;
+            let app = AnnatomicApp::new(cc, args)?;
             Ok(Box::new(app))
         }),
     )
