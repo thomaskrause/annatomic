e672ccad0242e5be7033fb0e2bae6876edaa4046
Thomas Krause <thomas.krause@hu-berlin.de>
2024-12-12 16:01:18 +0100

    Move corpus selection to the Project struct

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,19 @@
 use std::sync::Arc;
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
 use corpus_tree::CorpusTree;
 use egui::{Color32, RichText};
 use egui_modal::Modal;
-use graphannis::CorpusStorage;
 use job_executor::JobExecutor;
 use messages::Notifier;
-use project_manager::ProjectManager;
+use project::Project;
 use serde::{Deserialize, Serialize};
-use views::start::CorpusSelection;
 
 mod corpus_tree;
 mod job_executor;
 mod messages;
-mod project_manager;
+mod project;
 mod views;
 
 pub(crate) const APP_ID: &str = "annatomic";
@@ -35,23 +33,35 @@ pub struct AnnatomicArgs {
     dev: bool,
 }
 
-#[derive(Default, serde::Deserialize, serde::Serialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct AnnatomicApp {
     main_view: MainView,
-    corpus_selection: CorpusSelection,
     new_corpus_name: String,
-    project_manager: ProjectManager,
-    #[serde(skip)]
-    corpus_tree: Option<CorpusTree>,
+    project: Project,
     #[serde(skip)]
     jobs: Arc<JobExecutor>,
     #[serde(skip)]
     notifier: Arc<Notifier>,
     #[serde(skip)]
-    corpus_storage: Option<Arc<CorpusStorage>>,
-    #[serde(skip)]
     args: AnnatomicArgs,
+}
+
+impl Default for AnnatomicApp {
+    fn default() -> Self {
+        let notifier = Arc::new(Notifier::default());
+        let jobs = Arc::new(JobExecutor::default());
+        let project = Project::new(notifier.clone(), jobs.clone());
+
+        Self {
+            main_view: MainView::Start,
+            new_corpus_name: String::default(),
+            project,
+            jobs,
+            notifier,
+            args: AnnatomicArgs::default(),
+        }
+    }
 }
 
 impl AnnatomicApp {
@@ -65,9 +75,7 @@ impl AnnatomicApp {
         if let Some(storage) = cc.storage {
             let mut app: Self = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
             app.args = args;
-            app.ensure_corpus_storage_loaded()?;
-            // Rebuild the corpus state that is not persisted but calculated
-            app.schedule_corpus_tree_update();
+            app.project.load_after_init()?;
             return Ok(app);
         }
 
@@ -75,23 +83,9 @@ impl AnnatomicApp {
             args,
             ..Default::default()
         };
-        app.ensure_corpus_storage_loaded()?;
+        app.project.load_after_init()?;
 
         Ok(app)
-    }
-
-    fn ensure_corpus_storage_loaded(&mut self) -> anyhow::Result<Arc<CorpusStorage>> {
-        if let Some(cs) = &self.corpus_storage {
-            Ok(cs.clone())
-        } else {
-            let parent_path =
-                eframe::storage_dir(APP_ID).context("Unable to get local file storage path")?;
-            // Attempt to create a corpus storage and remember it
-            let cs = CorpusStorage::with_auto_cache_size(&parent_path.join("db"), true)?;
-            let cs = Arc::new(cs);
-            self.corpus_storage = Some(cs.clone());
-            Ok(cs)
-        }
     }
 
     fn handle_corpus_confirmation_dialog(&mut self, ctx: &egui::Context) {
@@ -99,7 +93,7 @@ impl AnnatomicApp {
         if modal.is_open() {
             modal.show(|ui| {
                 let corpus_name = self
-                    .corpus_selection
+                    .project
                     .scheduled_for_deletion
                     .clone()
                     .unwrap_or_default();
@@ -119,7 +113,7 @@ impl AnnatomicApp {
                         )
                         .clicked()
                     {
-                        self.corpus_selection.scheduled_for_deletion = None;
+                        self.project.scheduled_for_deletion = None;
                         modal.close();
                     }
                     if modal
@@ -130,54 +124,14 @@ impl AnnatomicApp {
                         )
                         .clicked()
                     {
-                        if let Some(cs) = self.corpus_storage.as_ref().cloned() {
-                            let title = format!("Deleting corpus \"{corpus_name}\"");
-                            self.jobs.add(
-                                &title,
-                                move |_job| {
-                                    cs.delete(&corpus_name)?;
-                                    Ok(())
-                                },
-                                |_result, _app| {},
-                            );
-                        }
-                        self.corpus_selection.scheduled_for_deletion = None;
+                        self.project.delete_corpus(corpus_name);
                         modal.close();
                     }
                 });
             });
         }
-        if self.corpus_selection.scheduled_for_deletion.is_some() {
+        if self.project.scheduled_for_deletion.is_some() {
             modal.open();
-        }
-    }
-
-    fn schedule_corpus_tree_update(&mut self) {
-        match self.ensure_corpus_storage_loaded() {
-            Ok(cs) => {
-                if let Some(corpus_name) = self.corpus_selection.name.clone() {
-                    // Run a background job that creates the new corpus structure
-
-                    let job_title = format!("Updating corpus structure for {}", &corpus_name);
-                    let notifier = self.notifier.clone();
-                    self.jobs.add(
-                        &job_title,
-                        move |_job| {
-                            let corpus_tree =
-                                CorpusTree::create_from_graphstorage(cs, &corpus_name, notifier)?;
-                            Ok(corpus_tree)
-                        },
-                        |result, app| {
-                            app.corpus_tree = Some(result);
-                        },
-                    );
-                } else {
-                    self.corpus_tree = None;
-                }
-            }
-            Err(err) => {
-                self.notifier.report_error(err);
-            }
         }
     }
 }
