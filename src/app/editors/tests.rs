1c3d480201bc4ef972748cc36534defa56121ae1
Thomas Krause <thomas.krause@hu-berlin.de>
2025-01-16 16:39:44 +0100

    Add test for changing the segmentation value

--- src/app/editors/tests.rs
+++ src/app/editors/tests.rs
@@ -1,7 +1,10 @@
 use std::sync::Arc;
 
-use egui::{mutex::RwLock, Vec2};
-use egui_kittest::Harness;
+use egui::{accesskit::Role, mutex::RwLock, Vec2};
+use egui_kittest::{
+    kittest::{Key, Node, Queryable},
+    Harness,
+};
 use graphannis::model::AnnotationComponentType;
 
 use crate::app::set_fonts;
@@ -62,4 +65,45 @@ fn render_segmentation_spans() {
     harness.run();
 
     harness.wgpu_snapshot("render_segmentation_spans");
+}
+
+#[test]
+fn change_segmentation_value() {
+    let (graph, _config) =
+        graphannis_core::graph::serialization::graphml::import::<AnnotationComponentType, _, _>(
+            &include_bytes!("../../../tests/data/SegmentationWithGaps.graphml")[..],
+            false,
+            |_| {},
+        )
+        .unwrap();
+    let document_node = graph
+        .get_node_annos()
+        .get_node_id_from_name("SegmentationWithGaps/doc01")
+        .unwrap()
+        .unwrap();
+    let job = JobExecutor::default();
+    let mut editor =
+        DocumentEditor::create_from_graph(document_node, Arc::new(RwLock::new(graph)), job.clone())
+            .unwrap();
+    let mut harness = Harness::builder().build_ui(move |ui| {
+        set_fonts(ui.ctx());
+        editor.show(ui);
+    });
+    harness.set_size(Vec2::new(900.0, 120.0));
+    harness.get_by_label("subtokenized").click();
+    harness.run();
+    get_text_input(&harness, "subtokenized").type_text("t");
+    harness.run();
+    get_text_input(&harness, "subtokenizedt").key_press(Key::Enter);
+    harness.run();
+
+    harness.wgpu_snapshot("change_segmentation_value");
+}
+
+fn get_text_input<'a>(harness: &'a Harness<'_>, value: &'a str) -> Node<'a> {
+    harness
+        .get_all_by_value(value)
+        .filter(|n| n.role() == Role::TextInput)
+        .next()
+        .unwrap()
 }
